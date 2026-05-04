f3820d4d37462e0cb300e118c4d0d76656d4a842
sreejith <sreejithsev@gmail.com>
2026-05-04 07:15:37 -0700

    fix: resolve plain text display and favicon 404 errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://.github.io',
-  base: '/my-portfolio',
+  site: 'https://sreejithsev.github.io',
+  base: '/',
   integrations: [tailwind()],
 });
